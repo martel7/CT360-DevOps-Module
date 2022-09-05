b9de7f93cb9e8a99ca3cf20dd6188764bd1d4be0
Dusan Markovic <dusan112233@gmail.com>
2022-09-05 16:38:33 +0200

    Made a simple OOP demo in Groovy using Java syntax

---
+++ oopGroovy.groovy
@@ -0,0 +1,59 @@
+class firstGroovyProgram{
+
+    static void main(String[] args){
+
+        Animal dog1 = new Dog();
+        dog1.setHumanAge(10);
+        dog1.setName("Dzeki");
+        System.out.println("Dog is named: " + dog1.getName());
+        System.out.println("Dog is " + dog1.getAge() + " year(s) old");
+        dog1.makeSound();
+    }
+}
+
+public class Animal{
+
+        int age;
+        String name;
+
+        public Animal(){
+            System.out.println("An instance of an animal was created with a hash code: " + this.hashCode());
+        }
+
+        public void setHumanAge(int age){
+            this.age = age * 7;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public int getAge(){
+            return this.age;
+        }
+
+        public String getName(){
+            return this.name;
+        }
+
+        public void makeSound(){
+            System.out.println("Default animal");
+        }
+    }
+
+public class Dog extends Animal{
+
+    public Dog(){
+        System.out.println("An instance of a dog was created with a hash code:     " + this.hashCode());
+    }
+
+    public void Dog(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println("Woof");
+    }
+}
